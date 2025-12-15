0551430a28e71dc3ad1707acf406d97cc7fe4f41
Trishan <sanjanatrishan@gmail.com>
2025-12-15 19:27:32 +0530

    Refactor CLI structure to encapsulate argument parsing and repository fetching within the main function

--- GitPulse/cli.py
+++ GitPulse/cli.py
@@ -1,47 +1,42 @@
 import argparse
-import utils
+from GitPulse import config, utils, github_api, formatter
 
-import config
-import formatter
-import github_api
+def main():
+    parser = argparse.ArgumentParser(
+        prog="GitPulse",
+        description="GitHub Repository Trending CLI Tool",
+        epilog="Thank you for using %(prog)s! :)",
+    )
 
-parser = argparse.ArgumentParser(
-    prog="GitPulse",
-    description="GitHub Repository Trending CLI Tool",
-    epilog="Thank you for using %(prog)s! :)",
-)
+    parser.add_argument(
+        "-d",
+        "--duration",
+        type=str,
+        choices=config.VALID_DURATIONS,
+        help="Time duration to filter repositories",
+        default=config.DEFAULT_DURATION,
+    )
+    parser.add_argument(
+        "-c", "--calendar", action="store_true", help="Use calendar-based date ranges"
+    )
 
-parser.add_argument("gitpulse", help="GitPulse CLI Tool")
+    parser.add_argument(
+        "-l", "--language", type=str, help="Programming language to filter repositories"
+    )
 
-parser.add_argument(
-    "-d",
-    "--duration",
-    type=str,
-    choices=config.VALID_DURATIONS,
-    help="Time duration to filter repositories",
-    default=config.DEFAULT_DURATION,
-)
-parser.add_argument(
-    "-c", "--calendar", action="store_true", help="Use calendar-based date ranges"
-)
+    parser.add_argument(
+        "--limit",
+        type=int,
+        help="Number of repositories to display",
+        default=config.DEFAULT_LIMIT,
+    )
 
-parser.add_argument(
-    "-l", "--language", type=str, help="Programming language to filter repositories"
-)
+    args = parser.parse_args()
 
-parser.add_argument(
-    "--limit",
-    type=int,
-    help="Number of repositories to display",
-    default=config.DEFAULT_LIMIT,
-)
+    date_range = utils.get_date_ranges(args.duration, args.calendar)
+    language = args.language
+    limit = args.limit
 
-args = parser.parse_args()
+    repos = github_api.fetch_repos(date_range, language, limit)
 
-date_range = utils.get_date_ranges(args.duration, args.calendar)
-language = args.language
-limit = args.limit
-
-repos = github_api.fetch_repos(date_range, language, limit)
-
-formatter.format_repos(repos)
+    formatter.format_repos(repos)
